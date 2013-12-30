ad07bac211b6caf073401dd5e975df9d5182e176
ke20 <k_auvinet@hotmail.fr>
2013-12-30 16:47:32 +0100

    initialize project

---
+++ manifests/initialize.pp
@@ -0,0 +1,56 @@
+
+Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
+
+exec { 'apt-get update':
+    command => 'apt-get update',
+}
+
+# -------------------------------------
+# Apache installation and configuration
+class { "apache": }
+
+apache::module { 'rewrite': }
+
+apache::vhost { 'default':
+    docroot     => '/vagrant/projects',
+    server_name => false,
+    priority    => '',
+    template    => 'apache/virtualhost/vhost.conf.erb',
+}
+
+
+# ----------------------------------
+# PHP installation and configuration
+class { 'php': }
+
+php::module { ["curl", "gd", "imagick", "xdebug"]: }
+
+# Override the PHP configuration
+file {'/etc/php5/conf.d/errors.conf':
+  ensure => present,
+  owner => root, group => root, mode => 444,
+  content => "display_errors = On \n",
+}
+
+
+# ------------------------------------
+# MySQL installation and configuration
+class { 'mysql':
+    root_password => 'root'
+}
+
+
+# -----------------------------------------
+# phpmyadmin installation and configuration
+package { 'phpmyadmin':
+    ensure  => latest,
+    require  => Exec['apt-get update'],
+}
+
+file { '/etc/apache2/conf.d/phpmyadmin.conf':
+    ensure  => link,
+    target  => '/etc/phpmyadmin/apache.conf',
+    require => Package['apache', 'phpmyadmin'],
+    notify  => Service["apache2"],
+}
+
